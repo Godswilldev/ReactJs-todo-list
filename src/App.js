adcf161c51ab1b20e17803e4d90dfe40d312b203
Godswilldev <gimmex1@gmail.com>
2021-05-18 16:45:54 +0100

    implemented Dark/Light mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React, { Component } from "react";
+
 import TodoList from "./Components/TodoList";
+import iconMoon from "../src/images/icon-moon.svg";
+import iconSun from "../src/images/icon-sun.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    darkMode: true,
+  };
+  handleClick = () => this.setState({ darkMode: !this.state.darkMode });
+
+  render() {
+    return (
+      <div className={this.state.darkMode ? "app app__dark" : "app app__light"}>
+        <div
+          className={
+            this.state.darkMode
+              ? "app__header app__header--dark"
+              : "app__header app__header--light"
+          }
+        >
+          <h1>Todo</h1>
+          <img
+            src={this.state.darkMode ? iconSun : iconMoon}
+            alt={this.state.darkMode ? "iconSun " : "iconMoon"}
+            onClick={this.handleClick}
+          />
+        </div>
+        <TodoList />
+      </div>
+    );
+  }
 }
-
 export default App;
